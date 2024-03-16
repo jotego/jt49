ac9f4a719d659fea5ebd15eb1a819d89e1271603
etriana85 <esperanza.duarte@jotego.es>
2024-03-16 08:43:19 +0100

    PSG output visible. AGND (pin 17) was not connected

--- lab/io/io.ino
+++ lab/io/io.ino
@@ -22,7 +22,7 @@ void wrReg( int r, int v) {
 	PORTB=0xfe; // A0 low
 	wrDout(r);
 	delay(1);
-	PORTB=0xff; // A0 hihg
+	PORTB=0xff; // A0 high
 	wrDout(v);
 	delay(1);
 }
@@ -33,13 +33,31 @@ void psg_setup() {
 	delay(100);
 	digitalWrite(ICPIN,1);
 	delay(100);
-	wrReg(7, 0xe0 ); // tone enabled, IO ports as outputs
+	for(int k=0;k<6;k+=2) { // set minimum freq for all channels
+		wrReg(0+k,(k<<2)+1);
+		wrReg(1+k,0x0);
+	}
+	// wrReg(0x2d,0); // pre scaler 658 kHz
+	// wrReg(0x2e,0);    // pre scaler 1.3 MHz
+	wrReg(0x21,0); 		// no test
+	wrReg(0x2f,0);    	// pre scaler 2.0 MHz, SSG=full clock
+	wrReg(7, 0xc0 ); 	// tone enabled, IO ports as outputs
+	wrReg(6, 0x10 ); 	// noise
+	wrReg( 010,0x7);
+	wrReg( 011, 6 );
+	wrReg( 012, 7 );
 }
 
+int cnt=1;
 void loop() {
-	for(int cnt=0;;cnt++) {
-		wrReg(0xe,cnt);
-		wrReg(0xf,~cnt);
+	cnt++;
+	// wrReg(0xe,cnt);
+	wrReg(0xf,cnt);	
+	if((cnt&0x1f)==0) {
+		wrReg(010,(cnt>>5)&0xf);
+		wrReg(011,(cnt>>5)&0xf);
+		wrReg(012,(cnt>>5)&0xf);
 	}
+	// delay(10);
 }
 
