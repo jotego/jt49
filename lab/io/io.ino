982156d75fe230885b9c477a0ab70e246e138858
Jose Tejada <jose.tejada@jotego.es>
2024-03-31 11:50:23 +0200

    Measured YM2203 output voltage for different loads (two chips)

--- lab/io/io.ino
+++ lab/io/io.ino
@@ -1,34 +1,30 @@
-const int A0PIN=8, WRPIN=9, ICPIN=10;
+const int A0PIN=8,  // PB0
+          WRPIN=9,  // PB1
+          ICPIN=10; // PB2
 
 void setup() {
-	// Set Pins 2-12 as an output
+	// Set ports B and D as an output
 	DDRD=0xff;
 	DDRB=0xff;
-	// pinMode(A0PIN, OUTPUT);
-	// pinMode(WRPIN, OUTPUT);
-	// pinMode(ICPIN, OUTPUT);
 	// PSG
 	psg_setup();
-	// Serial.begin(9600);
 }
 
-void wrDout(int v) {
-	PORTD=v&0xff;
-	PORTB=PINB&0xfd; // WR low
-	PORTB=PINB|0x02; // WR high
+void wrDout(int v, int a0 ) {
+	PORTD=v;
+	a0=(a0&1)|4;	// keep IC high (bit 2)
+	PORTB=a0; // WR low
+	PORTB=a0|0x02; // WR high
+	// delay(1);
 }
 
 void wrReg( int r, int v) {
-	PORTB=0xfe; // A0 low
-	wrDout(r);
-	// delay(1);
-	PORTB=0xff; // A0 high
-	wrDout(v);
-	// delay(1);
+	wrDout(r,0);
+	wrDout(v,1);
 }
 
 void psg_setup() {
-	// reset
+	// reset - if not c
 	digitalWrite(ICPIN,0);
 	delay(100);
 	digitalWrite(ICPIN,1);
@@ -45,7 +41,7 @@ void psg_setup() {
 	// wrReg(0x2e,0);    // pre scaler 1.3 MHz
 	wrReg(0x21,0); 		// no test
 	wrReg(0x2f,0);    	// pre scaler 2.0 MHz, SSG=full clock
-	wrReg(7, 0370 ); 	// tone enabled, IO ports as outputs
+	wrReg(7, 0376 ); 	// tone enabled, IO ports as outputs
 	// wrReg(7, 0334 ); 	// noise enabled, IO ports as outputs
 	wrReg(6, 0x1f ); 	// noise
 	wrReg( 010, 0 );
@@ -60,66 +56,9 @@ void psg_setup() {
 }
 
 void loop() {
-	int pot = analogRead(A0);
-	// wrReg( 010, pot>>6 );
+	int amp = analogRead(A0)>>6;
+	wrReg( 0xf, amp ); // show on port B	
+	wrReg( 010, amp ); // volume channel A
 	delay(20);
-}
-
-struct Delta{
-	int h, l;
-};
-
-void delta( struct Delta* d ) {
-	const int LEN=50;
-	int v[LEN];
-	long int h=0,l=0,hk=0,lk=0;
-	long int ave=0;
-	for( int k=0; k<LEN; k++ ) {
-		v[k] = analogRead(A0);
-		delay(random(25));
-		ave+=v[k];
-	}
-	ave/=LEN;
-	for( int k=0; k<LEN; k++ ) {
-		if(v[k]>ave) {
-			h+=v[k]; 
-			hk++;
-		} else {
-			l+=v[k];
-			lk++;
-		}
-	}
-	if(hk==0||lk==0) {
-		d->h=0;
-		d->l=0;
-		return;
-	}
-	d->h = h/hk;
-	d->l = l/lk;
-}
-
-void dump( struct Delta *d, int len ) {
-	char str[40];
-	Serial.begin(9600);	
-	for( int k=0; k<len; k++ ) {
-		sprintf(str,"%d,%d,%d,%d",k,d[k].l,d[k].h,d[k].h-d[k].l);	
-		Serial.println(str);
-	}
-	Serial.end();
-}
-
-void sweep_all() {
-	struct Delta d[16*3];
-	int ch=0;
-	for( ch=0; ch<3; ch++ ) {
-		wrReg(010+ch,0);
-	}
-	for( ch=0; ch<3; ch++ ) {
-		for( int vol=0; vol<16; vol++ ) {
-			wrReg(010+ch,vol&0xf);
-			delta(&d[vol+(ch<<4)]);
-		}
-	}
-	dump(d,sizeof(d)/sizeof(Delta));
 }
 
